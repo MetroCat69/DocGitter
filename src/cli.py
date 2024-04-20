77376b7ab4396376fcdc2c2dbd0109a0878f9fcd
MetroCat69 <aaaaa>
2024-04-20 15:27:24 +0300

    added create new project and list project and delete

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,36 @@
 import click
+import os
 from config import glitter_init
+from glitter_project import create_new_project_from_remote_url,delete_project_and_remove_config,list_created_projects
 
+@click.group()
+def cli():
+    pass
 
-@click.argument("a", type=click.FLOAT)
-@click.argument("b", type=click.FLOAT)
+@cli.command()
 def init():
     glitter_init()
+
+@cli.command()
+@click.argument('remote_url')
+@click.option('--glitter_project_path', default=os.path.join(os.path.expanduser('~'), '.glitter'), help='Path where Glitter projects will be stored')
+def create_project(remote_url, glitter_project_path):
+    click.echo("Creating project from remote URL: {}".format(remote_url))
+    create_new_project_from_remote_url(remote_url, glitter_project_path)
+
+@cli.command()
+@click.argument('project_name',type=click.STRING)
+@click.option('--glitter_project_path', default=os.path.join(os.path.expanduser('~'), '.glitter'), help='Path where Glitter projects will be stored')
+def delete_project(project_name, glitter_project_path):
+    click.echo("Deleting project : {}".format(project_name))
+    delete_project_and_remove_config(project_name, glitter_project_path)
+
+
+@cli.command()
+@click.option('--glitter_project_path', default=os.path.join(os.path.expanduser('~'), '.glitter'), help='Path where Glitter projects will be stored')
+def list_project(glitter_project_path):
+    projects = list_created_projects(glitter_project_path)
+    click.echo("project : {}".format(projects))
+
+if __name__ == '__main__':
+    cli()
--- src/glitter_project.py
+++ src/glitter_project.py
@@ -1,8 +1,11 @@
 import os
 from git import Repo
 import logging
-from src.config import update_glitter_config, glitter_init
-
+from config import update_glitter_config, glitter_init,glitter_config_path
+from exception import ProjectExistsError
+import mammoth
+import shutil
+import json
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -10,13 +13,86 @@ logger = logging.getLogger(__name__)
 def create_new_project_from_remote_url(remote_url: str, glitter_project_path: str) -> str:
     project_name = remote_url.split('/')[-1].split('.')[0]
     project_dir = os.path.join(glitter_project_path, project_name)
-    
-    if not os.path.exists(project_dir):
+    config_path = glitter_config_path(glitter_project_path)
+
+    if os.path.exists(project_dir):
+        logger.warning(f"Project '{project_name}' already exist. at {project_dir}")
+        return None
+    if not os.path.exists(glitter_project_path):
         glitter_init()
     
     Repo.clone_from(remote_url, project_dir)
-    update_glitter_config(os.path.join(project_dir, "glitter_config.json"), remote_url)
+    update_glitter_config(config_path, remote_url)
     
     logger.info("New project directory created at: %s", project_dir)
     logger.info("Remote repository with URL: %s", remote_url)
     return project_dir
+
+
+def delete_project_and_remove_config(project_name: str, glitter_project_path: str):
+    project_dir = os.path.join(glitter_project_path, project_name)
+    config_path = glitter_config_path(glitter_project_path)
+
+    if not os.path.exists(project_dir):
+        logger.warning(f"Project '{project_name}' does not exist.")
+        return None
+
+    try:
+        shutil.rmtree(project_dir)
+        logger.info(f"Project directory '{project_dir}' has been deleted.")
+    except Exception as e:
+        logger.error(f"Failed to delete project directory '{project_dir}': {e}")
+        return
+
+    # Update the Glitter configuration
+    try:
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+
+        config['projects'] = {key: value for key, value in config['projects'].items() if key != project_name}
+
+        with open(config_path, 'w') as f:
+            json.dump(config, f, indent=2)
+
+        logger.info(f"Project '{project_name}' has been removed from the Glitter configuration.")
+    except Exception as e:
+        logger.error(f"Failed to update Glitter configuration: {e}")
+
+def list_created_projects(glitter_project_path: str):
+     config_path = glitter_config_path(glitter_project_path)
+     with open(config_path, 'r') as f:
+            config = json.load(f)
+     return [p for p in config['projects'].keys()]
+
+
+
+
+
+def convert_docx_to_md(input_path,saved_file_path):
+    # Create the cache directory if it doesn't exist
+    cache_dir = Path.home() / ".glitter"
+    cache_dir.mkdir(parents=True, exist_ok=True)
+
+    for root, dirs, files in os.walk(input_path):
+        # Create the corresponding directory structure in the cache
+        relative_path = os.path.relpath(root, input_path)
+        cache_root = cache_dir / relative_path
+
+        # Create the directory if it doesn't exist
+        cache_root.mkdir(parents=True, exist_ok=True)
+
+        for file in files:
+            if file.endswith(".docx"):
+                input_file = os.path.join(root, file)
+                output_file = os.path.join(cache_root, os.path.splitext(file)[0] + ".md")
+
+                # Convert the .docx file to Markdown
+                with open(input_file, "rb") as docx_file:
+                    result = mammoth.convert_to_markdown(docx_file)
+                    markdown = result.value
+
+                # Save the Markdown content to a file
+                with open(output_file, "w", encoding="utf-8") as md_file:
+                    md_file.write(markdown)
+
+                print(f"Converted {input_file} to {output_file}")
