2d3947b02acf6b6bcd167b73a27ece3ddae402e4
MetroCat69 <aaaaa>
2024-04-22 13:22:27 +0300

    somelinting + linting configuration

--- src/cli.py
+++ src/cli.py
@@ -1,24 +1,33 @@
 import click
-import os
 from config import glitter_init
-from glitter_project import create_new_project_from_remote_url,delete_project_and_remove_config,list_created_projects,add_and_update_files,move_current_project
+from glitter_project import (
+    create_new_project_from_remote_url,
+    delete_project_and_remove_config,
+    list_created_projects,
+    add_and_update_files,
+    move_current_project,
+)
+
 
 @click.group()
 def cli():
     pass
 
+
 @cli.command()
 def init():
     glitter_init()
 
+
 @cli.command()
-@click.argument('remote_url')
+@click.argument("remote_url")
 def create_project(remote_url):
     click.echo("Creating project from remote URL: {}".format(remote_url))
     create_new_project_from_remote_url(remote_url)
 
+
 @cli.command()
-@click.argument('project_name',type=click.STRING)
+@click.argument("project_name", type=click.STRING)
 def delete_project(project_name):
     click.echo("Deleting project : {}".format(project_name))
     delete_project_and_remove_config(project_name)
@@ -26,21 +35,22 @@ def delete_project(project_name):
 
 @cli.command()
 def list_projects():
-    current_project,projects = list_created_projects()
-    if not (current_project ==None and projects==None):
+    current_project, projects = list_created_projects()
+    if not (current_project is None and projects is None):
         click.echo(f"Current project: {current_project} \n projects : {projects}")
 
 
 @cli.command()
-@click.argument('input_path')
+@click.argument("input_path")
 def add(input_path):
     add_and_update_files(input_path)
 
 
 @cli.command()
-@click.argument('project_name')
+@click.argument("project_name")
 def change_current_project(project_name):
     move_current_project(project_name)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
--- src/file_conversion_handler.py
+++ src/file_conversion_handler.py
@@ -1,21 +1,7 @@
-from  file_convertors.docs_convertor import convert_docx_to_markdown
+from file_convertors.docs_convertor import convert_docx_to_markdown
 import os
-import mammoth
 
-def convert_docx_to_markdown(input_file_path,output_file_path):
-
-    with open(input_file_path, "rb") as docx_file:
-        result = mammoth.convert_to_markdown(docx_file)
-        markdown = result.value
-
-    with open(output_file_path+".md", "w", encoding="utf-8") as md_file:
-        md_file.write(markdown)
-
-
-conversion_config_dict = {
-    ".docx"  : convert_docx_to_markdown
-}
-
+conversion_config_dict = {".docx": convert_docx_to_markdown}
 
 
 def convert_files(input_path, output_path):
@@ -26,15 +12,18 @@ def convert_files(input_path, output_path):
             relative_output_path = os.path.relpath(input_file_path, input_path)
             output_file_path = os.path.join(output_path, relative_output_path)
 
-            conversion_func = conversion_config_dict.get(os.path.splitext(input_file_path)[1])
+            conversion_func = conversion_config_dict.get(
+                os.path.splitext(input_file_path)[1]
+            )
 
             if conversion_func:
 
                 output_file_dir = os.path.dirname(output_file_path)
-                print(output_file_dir)
-                output_file_basename = os.path.splitext(os.path.basename(output_file_path))[0]
-                output_file_path = os.path.join(output_file_dir,output_file_basename)
-                
+                output_file_basename = os.path.splitext(
+                    os.path.basename(output_file_path)
+                )[0]
+                output_file_path = os.path.join(output_file_dir, output_file_basename)
+
                 if not os.path.exists(output_file_dir):
                     os.makedirs(output_file_dir)
 
